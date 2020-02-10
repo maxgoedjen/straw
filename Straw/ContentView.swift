03930373dd92f9de367bf9642ae693f0ac0147c1
Max Goedjen <max.goedjen@gmail.com>
2020-02-09 16:25:41 -0800

    Add project

---
+++ Straw/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Straw
+//
+//  Created by Max Goedjen on 2/9/20.
+//  Copyright © 2020 Max Goedjen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
